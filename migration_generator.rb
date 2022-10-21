459f74556a0de0264290e970b3e41db50a8c5c2c
Richard <113485471+dsuprmajstc93r@users.noreply.github.com>
2022-10-21 05:34:03 -0600

    Add files via upload
    Signed-off-by: Richard <113485471+dsuprmajstc93r@users.noreply.github.com>

--- migration_generator.rb
+++ migration_generator.rb
@@ -1,12 +1,8 @@
 # frozen_string_literal: true
 
-require "rails/generators/model_helpers"
-
 module Rails
   module Generators
-    class ModelGenerator < NamedBase # :nodoc:
-      include Rails::Generators::ModelHelpers
-
+    class MigrationGenerator < NamedBase # :nodoc:
       argument :attributes, type: :array, default: [], banner: "field[:type][:index] field[:type][:index]"
       hook_for :orm, required: true, desc: "ORM to be invoked"
     end
